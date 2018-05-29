8158ab26495b958e86505f4384262359a98feac5
TheCompSciNoob <kyroschow@gmail.com>
2018-05-28 17:54:07 -0700

    Update ServerController.kt

--- src/ServerController.kt
+++ src/ServerController.kt
@@ -5,3 +5,25 @@ fun main(args: Array<String>) {
 }
 
 external fun require(module: String): dynamic
+
+/*
+import kotlin.js.Json
+
+external class Server {
+    fun listen(port: Int, callback: () -> Unit)
+}
+
+external class IO {
+    fun on(event: String, callback: (Socket) -> Unit)
+}
+
+external class Socket {
+    fun on(event: String, callback: (dynamic) -> Unit)
+
+    fun emit(name: String, info: dynamic)
+
+    var id: String
+
+    var broadcast: Socket
+}
+*/
